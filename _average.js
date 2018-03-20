2bd67c2d29f3796f395c1465ac66aaa4a174eec7
dragmove <dragmove@naver.com>
2018-03-21 00:11:56 +0900

    add _sum, _variance, _util

--- _average.js
+++ _average.js
@@ -1,5 +1,10 @@
+import { not } from './_util';
+import sum from './_sum';
+
 function average(array) {
-  return array.reduce((previous, current) => previous + current, 0) / array.length;
+  if (not(Array.isArray)(array)) throw new TypeError('average : array parameter type must be Array.');
+
+  return (array.length <= 0) ? 0 : sum(array) / array.length;
 }
 
 export default average;
---
+++ _variance.js
@@ -0,0 +1,15 @@
+import { not } from './_util';
+import average from './_average';
+
+function variance(array) {
+  if (not(Array.isArray)(array)) throw new TypeError('variance : array parameter type must be Array.');
+
+  if (array.length <= 0) return 0;
+
+  const av = average(array),
+    sum = array.reduce((previous, current) => previous + Math.pow(current - av, 2), 0);
+
+  return sum / (array.length - 1);
+}
+
+export default variance;
