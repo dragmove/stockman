9ddf4894cbdafc5677f22a4428f3ca365ae4971a
김현석 <dragmove@naver.com>
2018-04-09 09:33:32 +0900

    feat: Change ava config option, update test.sum.js

--- sum.js
+++ sum.js
@@ -4,6 +4,8 @@ export function sum(array) {
   if (not(Array.isArray)(array)) throw new TypeError('[sum] : array parameter type must be Array.');
 
   return (array.length <= 0) ? 0 : array.reduce(function (previous, current) {
+    // TODO: if current value is not number type, throw type error.
+
     return previous + current;
   }, 0);
 }
--- tests/test.sum.js
+++ tests/test.sum.js
@@ -1,6 +1,54 @@
 import test from 'ava';
-import sum from '../sum';
+import { sum } from '../sum';
 
-test('sum', t => {
-  console.log('t :', t);
+/*
+ test.before(t => {
+ console.log('before test sum');
+ });
+ */
+
+test('if argument is not Array type, throw TypeError.', t => {
+  const errorMessage = '[sum] : array parameter type must be Array.';
+
+  let error = t.throws(() => sum(undefined), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(null), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(false), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(true), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(''), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum({}), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(function() {}), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(new RegExp('^stockman')), TypeError);
+  t.is(error.message, errorMessage);
+
+  error = t.throws(() => sum(new RegExp(/^stockman/)), TypeError);
+  t.is(error.message, errorMessage);
 });
+
+test('if argument Array has no element, return 0.', t => {
+  t.is(sum([]), 0);
+});
+
+test('if argument Array has elements, return sum', t => {
+  t.is(sum([1, 2, 3, 4, 5]), 15);
+});
+
+
+/*
+ test.after('cleanup', t => {
+ console.log('after test sum');
+ });
+ */
